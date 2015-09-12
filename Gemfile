2cb157229d047ce0cb53ca0a4d2b8793d33a2adf
Roman Vakulchik <r.vakulchik@sumatosoft.com>
2015-09-13 01:18:54 +0300

    initialize project

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'cucumber', '~> 2.1.0'
+gem 'capybara', '~> 2.5.0'
---
+++ features/step_definitions/find.rb
@@ -0,0 +1,19 @@
+Given(/^I am on Belarus railway page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will search for data$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see 'Маршрут следования'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will click the necessary train$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see number in the column 'Номер вагона'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
